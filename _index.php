7708b7b07950206dd0081bc370c105e6490dd86e
Mark Baumann <kontakt@markb.de>
2021-12-01 21:19:27 +0100

    Create _index.php

---
+++ _index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+	<title>Login Form</title>
+</head>
+<body>
+	<div class="container">
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email"  required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password"  required>
+			</div>
+			<div class="input-group">
+                <br>
+				<button name="submit" class="btn btn-dark">Login</button>
+			</div>
+			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+		</form>
+	</div>
+</body>
+</html>
